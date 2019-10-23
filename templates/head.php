ed6da8b783013969e57c00911b1189735b546575
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-23 01:53:43 -0600

    Encabezados comunes

---
+++ templates/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $title; ?></title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/flexboxgrid.min.css">
+    <link rel="stylesheet" href="css/primary.css">
+</head>
+<body>
