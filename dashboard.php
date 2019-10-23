926723c5d584092effac23eac3cfe954271c1f37
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-23 01:55:38 -0600

    Se inicio creacion de diseño de dashboard

---
+++ dashboard.php
@@ -0,0 +1,43 @@
+<?php
+    $title = "Dashboard - UPES";
+
+    include 'templates/head.php';
+?>
+
+    <section class="navBar container-fluid">
+        <div class="row middle-xs">
+            <div class="col-xs-12 col-sm-6 col-md-6 center-xs start-sm">
+                <!-- <img class="logo_img" src="img/upes.png" alt="logo" width="50px"> -->
+                <h2 class="logo_text">UPES</h2>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="row center-xs end-sm middle-xs">
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
+                        <button><i class="material-icons color_white">dashboard</i></button>
+                    </div>
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
+                        <button><i class="material-icons color_white">bar_chart</i></button>
+                    </div>
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs hor_separator">
+                        <button><i class="material-icons color_white">chat</i></button>
+                    </div>
+                    <div class="hidden-xs col-sm-1 col-md-2 center-xs">
+                        <p class="margin_none color_white">Lorenzo López</p>
+                    </div>
+                    <div class="col-xs-2 col-sm-2 col-md-1 center-xs">
+                        <div class="img_profile">
+                            <img src="img/default.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="content container-fluid">
+        <h3>BIENVENIDO</h3>
+    </section>
+
+<?php
+    include 'templates/end.php';
+?>
