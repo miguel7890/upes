05d1ada12b547c1d5f58670c35c5d745ec8beeb4
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-23 23:48:57 -0600

    Se creo el footer comun para usuarios

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="container-fluid">
+        <div class="row middle-xs">
+            <div class="col-xs-3 col-sm-3 col-md-1 center-xs">
+                <img src="img/upes.png" width="50%">
+            </div>
+            <div class="col-xs-9 col-sm-9 col-md-5">
+                <h3 class="margin_none">Contactanos</h3>
+                <p class="margin_none"><b>E-mail: </b>example@upes.edu.sv</p>
+                <p class="margin_none"><b>Teléfono: </b>2231-0000 2233-0020</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 center-xs end-sm">
+                <button><img src="img/fb.png" width="40px" height="40px"></button>
+                <button><img src="img/tw.png" width="40px" height="40px"></button>
+            </div>
+        </div>
+    </div>
+</footer>
