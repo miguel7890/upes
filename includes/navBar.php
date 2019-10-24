ecf39bc538f09661b42bee0f22e85e39918c676c
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-23 23:48:13 -0600

    Se agregaron estilos para la foto de perfil y se cambio el ancho de la barra

--- includes/navBar.php
+++ includes/navBar.php
@@ -1,26 +1,28 @@
-<section class="navBar container-fluid">
-    <div class="row middle-xs">
-        <div class="col-xs-12 col-sm-6 col-md-6 center-xs start-sm">
-            <!-- <img class="logo_img" src="img/upes.png" alt="logo" width="50px"> -->
-            <h2 class="logo_text">UPES</h2>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-md-6">
-            <div class="row center-xs end-sm middle-xs">
-                <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
-                    <button><i class="material-icons color_white">dashboard</i></button>
-                </div>
-                <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
-                    <button><i class="material-icons color_white">bar_chart</i></button>
-                </div>
-                <div class="col-xs-2 col-sm-1 col-md-1 center-xs hor_separator">
-                    <button><i class="material-icons color_white">chat</i></button>
-                </div>
-                <div class="hidden-xs col-sm-1 col-md-2 center-xs">
-                    <p class="margin_none color_white">Lorenzo López</p>
-                </div>
-                <div class="col-xs-2 col-sm-2 col-md-1 center-xs">
-                    <div class="img_profile">
-                        <img src="img/default.jpg">
+<section class="navBar">
+    <div class="container-fluid">
+        <div class="row middle-xs">
+            <div class="col-xs-12 col-sm-6 col-md-6 center-xs start-sm">
+                <!-- <img class="logo_img" src="img/upes.png" alt="logo" width="50px"> -->
+                <h2 class="logo_text">UPES - <span class="color_white" id="hour"></span></h2>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="row center-xs end-sm middle-xs">
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
+                        <button><i class="material-icons color_white">dashboard</i></button>
+                    </div>
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs">
+                        <button><i class="material-icons color_white">bar_chart</i></button>
+                    </div>
+                    <div class="col-xs-2 col-sm-1 col-md-1 center-xs hor_separator">
+                        <button><i class="material-icons color_white">chat</i></button>
+                    </div>
+                    <div class="hidden-xs col-sm-1 col-md-2 center-xs">
+                        <p class="margin_none color_white">Lorenzo López</p>
+                    </div>
+                    <div class="col-xs-2 col-sm-2 col-md-1 center-xs">
+                        <div class="img_profile">
+                            <img src="img/default.jpg">
+                        </div>
                     </div>
                 </div>
             </div>
