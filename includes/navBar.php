18f32dd97fed5318f5df7f901bedc8515a85f925
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-24 16:39:37 -0600

    Solucione problema de responsividad de nombre de usuario

--- includes/navBar.php
+++ includes/navBar.php
@@ -18,7 +18,7 @@
                     <div class="col-xs-2 col-sm-1 col-md-1 center-xs hor_separator">
                         <button><i class="material-icons color_white">chat</i></button>
                     </div>
-                    <div class="hidden-xs col-sm-1 col-md-2 center-xs">
+                    <div class="hidden-xs hidden-sm col-md-2 center-xs">
                         <p class="margin_none color_white">Lorenzo López</p>
                     </div>
                     <div class="col-xs-2 col-sm-2 col-md-1 center-xs">
