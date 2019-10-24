a5a5d756c5b2aabddd81df145ef673a51e2cf422
miguel7890 <miguelantonio357@hotmail.com>
2019-10-24 16:30:04 -0600

    creo course.php
    se creo a intervas para cada curso individual, aun sin el backend.

---
+++ course.php
@@ -0,0 +1,177 @@
+<?php
+$title = "Curso - UPES";
+
+include 'includes/head.php';
+include 'includes/navBar.php';
+
+ ?>
+
+<section class="content">
+    <div class="container-fluid">
+        <section class="row box_separator">
+            <section class="col-xs-12 col-sm-3 col-md-2 center-xs">
+                <div class="img_profile_curso inline_block">
+                <img src="img/default.jpg" >     
+                </div>
+            </section>
+            <section class="col-xs-12 col-sm-9 col-md-10">
+                <div style="margin-top:30px;">  
+                <p class="margin_none color_tertiary center-xs start-sm">
+                IDW000019
+                </p>
+                <div class="color_secondary ">
+                 <h1 class="titulo_cursos center-xs start-sm">Introduccion al desarrollo web - G01 </h1>   
+                </div>
+                <p class="color_subtitle center-xs start-sm">
+                Oliver Rivas Menjivar - 03:00 PM
+                </p>
+                </div>
+            </section>
+        </section>
+    </div>
+    <div class="container-fluid">
+        <section class="row">
+            <div class="col-xs-12">
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">dashboard</i> 
+                        </div>
+                        <div class="col-xs-9 col-md-9">
+                        <p class="margin_none">Pagina Principal</p>  
+                        </div>
+                    </div>    
+                </button>
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">folder</i> 
+                        </div>
+                        <div class="col-xs-9 col-md-9">
+                        <p class="margin_none">Documentos</p>  
+                        </div>
+                    </div>    
+                </button>
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">device_hub</i> 
+                        </div>
+                        <div class="col-xs-6 col-md-9">
+                        <p class="margin_none">Secuencias de aprendizaje</p>  
+                        </div>
+                    </div>    
+                </button>
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">chat_bubble</i>
+                        </div>
+                        <div class="col-xs-9 col-md-9">
+                        <p class="margin_none">Foros</p>  
+                        </div>
+                    </div>    
+                </button>
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">group</i>
+                        </div>
+                        <div class="col-xs-9 col-md-9">
+                        <p class="margin_none">Grupos</p>  
+                        </div>
+                    </div>    
+                </button>
+                <button class="btn_option">
+                    <div class="row middle-xs center-xs">
+                        <div class="col-xs-3 col-md-3">
+                        <i class="material-icons">emoji_people</i>
+                        </div>
+                        <div class="col-xs-9 col-md-9">
+                        <p class="margin_none">Participantes</p>  
+                        </div>
+                    </div>    
+                </button>
+            </div>
+        </section>
+        <hr>
+        <div class="row">
+           <div class="col-xs-12 col-sm-12 col-md-6">
+               <div class="box_default">
+                   <h3 class="box_title down_separator">DESCRIPCIÓN</h3>
+                   <h4 class="box_subtitle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+                   <p>
+                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ex facere eius aut molestiae expedita sint suscipit rem a recusandae sunt dolore accusamus reprehenderit voluptatem sequi esse tempore, aliquam minima!
+                   </p>
+                   <div class="img_preview">
+                     <img src="img/banner.jpg">  
+                   </div>
+                   <div class="center-xs box_separator">
+                       <button class="btn_icon_primary"><i class="material-icons">more_horiz</i></button>
+                   </div>
+               </div>
+               <div class="box_default">
+                   <h3 class="box_title down_separator">ANUNCIOS</h3>
+                   <div class="box_default">
+                    <p class="color_tertiary">
+                        Cel100 - 2019-10-17 08:00:00 PM
+                    </p>
+                    <h4 class="box_subtitle"> Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+                   <p>
+                       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat ex facere eius aut molestiae expedita sint suscipit rem a recusandae sunt dolore accusamus reprehenderit voluptatem sequi esse tempore, aliquam minima!
+                   </p>
+                   </div>
+               </div>                     
+           </div> 
+           <div class="col-xs-12 col-sm-12 col-md-6">
+                <div class="box_default">
+                        <h3 class="box_title down_separator">TRABAJOS</h3>
+                        <div class="box_separator">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                    <th>Titulo</th>
+                                    <th>Tipo</th>
+                                    <th>Inicio</th>
+                                    <th>Limite</th>
+                                    </tr>
+                                    <tr>
+                                    <td>Primer sitio web con HTML y CSS</th>
+                                    <td><i class="material-icons">person</i></td>
+                                    <td>2020-10-19 10:00:00 AM</td>
+                                    <td>2020-10-26 22:00:00 PM</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                </div>
+                    </div>
+                    <div class="box_default">
+                        <h3 class="box_title down_separator">FOROS</h3>
+                        <div class="box_separator">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                    <th>Titulo</th>
+                                    <th>Mensajes</th>
+                                    <th>Inicio</th>
+                                    <th>Limite</th>
+                                    </tr>
+                                    <tr>
+                                    <td>Evaluacion de HTML desde su nacimiento.</th>
+                                    <td>4</td>
+                                    <td>2020-10-19 10:00:00 AM</td>
+                                    <td>2020-10-26 22:00:00 PM</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>             
+           </div> 
+        </div>
+    </div>
+</section>
+
+<?php
+    include 'includes/footer.php';
+    include 'includes/end.php';
+?>
