f0d4076fc2cdb49bf5d7331c18554620aed745bb
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-26 21:21:36 -0600

    Ahora se puede iniciar sesión y registrarse, antes debe crearse la base de datos que está en resources/

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+    session_start();
+    include 'fn/conexion.php';
+    include 'fn/users.php';
+
+    $user = new user();
+
+    if(!empty($_SESSION)){
+        header('location:dashboard.php');
+    }
+
+        $nombre=""; $apellido=""; $carnet=""; $usuario=""; $correo=""; $slack="";  $foto="";
+
+    if(!empty($_POST)){
+        if($_POST['type'] == 1){
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $carnet = $_POST['carnet'];
+            $usuario = $_POST['usuario'];
+            $correo = $_POST['correo'];
+            $pass = $_POST['pass'];
+            $rpass = $_POST['rpass'];
+            $slack = $_POST['slack'];
+            $foto = $_FILES['foto'];
+
+            $user->register($nombre,$apellido,$carnet,$usuario,$correo,$pass,$rpass,$slack,$foto);
+        } else {
+            $usuario = $_POST['usuario'];
+            $pass = $_POST['pass'];
+            $user->login($usuario,$pass);
+            $err = $user->getErrors();
+            if( count($err) > 0 ){
+                echo '<script>alert("'.$err[0].'");</script>';
+            }
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -24,17 +63,18 @@
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 center-xs end-sm">
-                    <form action="">
+                    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST">
                         <div class="row middle-xs">
                             <div class="col-xs-5 col-sm-5 col-md-5 center-xs">
-                                <input type="text" class="input_primary" placeholder="Usuario">
+                                <input type="text" class="input_primary" name="usuario" placeholder="Usuario">
                             </div>
                             <div class="col-xs-5 col-sm-5 col-md-5 center-xs">
-                                <input type="text" class="input_primary" placeholder="Contraseña">
+                                <input type="password" class="input_primary" name="pass" placeholder="Contraseña">
                             </div>
                             <div class="col-xs-2 col-sm-2 col-md-2 center-xs">
                                 <button class="btn_icon_primary"><i class="material-icons">send</i></button>
                             </div>
+                            <input type="hidden" name="type" value="0">
                         </div>  
                     </form>
                 </div>
@@ -57,46 +97,55 @@
                 <div class="row center-xs">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <h3 class="color_primary">INSCRIPCIÓN</h3>
-                        <form action="">                        
+                            <?php 
+                                if(!empty($_POST)){
+                                    echo '<div class="box_alert start-xs">
+                                    <ul>';
+                                    $err = $user->getErrors();
+                                    for ($i=0; $i < count($err); $i++) { 
+                                        echo '<li>'.$err[$i].'</li>';
+                                    }
+                                    echo '</ul>
+                                    </div>';
+                                }                                
+                            ?>
+                        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST"  enctype="multipart/form-data">
                             <div>
-                                <input type="text" class="input_primary" placeholder="Nombres">
+                                <input type="text" class="input_primary" name="nombre" placeholder="Nombres" value="<?php echo $nombre; ?>">
                             </div>
                             <div>
-                                <input type="text" class="input_primary" placeholder="Apellidos">
+                                <input type="text" class="input_primary" name="apellido" placeholder="Apellidos" value="<?php echo $apellido; ?>">
                             </div>
                             <div>
-                                <input type="text" class="input_primary" placeholder="Carnet" maxlength="6">
+                                <input type="text" class="input_primary" name="carnet" placeholder="Carnet" maxlength="8" value="<?php echo $carnet; ?>">
                             </div>
                             <div>
-                                <input type="text" class="input_primary" placeholder="Usuario">
+                                <input type="email" class="input_primary" name="correo" placeholder="Correo" value="<?php echo $correo; ?>">
                             </div>
                             <div>
-                                <input type="password" class="input_primary" placeholder="Contraseña">
+                                <input type="text" class="input_primary" name="usuario" placeholder="Usuario" value="<?php echo $usuario; ?>">
                             </div>
                             <div>
-                                <input type="password" class="input_primary" placeholder="Repetir contraseña">
+                                <input type="password" class="input_primary" name="pass" placeholder="Contraseña">
                             </div>
                             <div>
-                                <input type="email" class="input_primary" placeholder="Correo">
+                                <input type="password" class="input_primary" name="rpass" placeholder="Repetir contraseña">
                             </div>
                             <div>
-                                <input type="text" class="input_primary" placeholder="Cuenta Slack">
+                                <input type="text" class="input_primary" name="slack" placeholder="Cuenta Slack" value="<?php echo $slack; ?>">
                             </div>
                             <div>
-                                <input type="text" class="input_primary" placeholder="Teléfono" maxlength="9">
-                            </div>
-                            <div>
-                                <input type="file" class="input_primary" placeholder="Foto">
+                                <input type="file" class="input_primary" name="foto" placeholder="Foto">
                             </div>
                             <div class="row">
                                 <div class="col-xs-6">
                                     <a href="./" class="btn_link_secondary">Cancelar</a>
                                 </div>
                                 <div class="col-xs-6">
-                                    <button class="btn_text_primary">Inscribirse</button>
+                                    <button class="btn_text_primary" type="submit">Inscribirse</button>
                                 </div>
-                                
                             </div>
+                            <input type="hidden" name="type" value="1">
                         </form>
                     </div>
                 </div>
