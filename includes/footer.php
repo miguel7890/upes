fcc492b22c2894d1897eec25b5aa9518107483c0
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-26 21:38:20 -0600

    Cambios en la grilla para una mejor presentación en dispositivos moviles

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
 <footer>
     <div class="container-fluid">
         <div class="row middle-xs">
-            <div class="col-xs-3 col-sm-3 col-md-1 center-xs">
+            <div class="col-xs-2 col-sm-3 col-md-1 center-xs">
                 <img src="img/upes.png" width="50%">
             </div>
-            <div class="col-xs-9 col-sm-9 col-md-5">
+            <div class="col-xs-6 col-sm-9 col-md-5">
                 <h3 class="margin_none">Contactanos</h3>
                 <p class="margin_none"><b>E-mail: </b>example@upes.edu.sv</p>
                 <p class="margin_none"><b>Teléfono: </b>2231-0000 2233-0020</p>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-6 center-xs end-sm">
+            <div class="col-xs-4 col-sm-12 col-md-6 center-xs end-sm">
                 <button><img src="img/fb_w.png" width="40px" height="40px"></button>
                 <button><img src="img/tw_w.png" width="40px" height="40px"></button>
             </div>
