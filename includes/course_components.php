b92e97ad3401e6e857ec713f2dd785560976fcbf
LorenzoLopez <lorenzolopezdepaz@gmail.com>
2019-10-26 21:34:20 -0600

    Archivo que enlista los componentes del curso

---
+++ includes/course_components.php
@@ -0,0 +1,40 @@
+<section class="row">
+    <div class="col-xs-12"> 
+        <a href="course.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">dashboard</i> 
+                <p class="margin_none">Pagina Principal</p>  
+            </div>    
+        </button></a>
+        <a href="documents.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">folder</i> 
+                <p class="margin_none">Documentos</p>  
+            </div>    
+        </button></a>
+        <a href="sequence.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">device_hub</i> 
+                <p class="margin_none">Secuencias de aprendizaje</p>  
+            </div>    
+        </button></a>
+        <a href="forums.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">chat_bubble</i> 
+                <p class="margin_none">Foros</p>  
+            </div>    
+        </button></a>
+        <a href="groups.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">group</i> 
+                <p class="margin_none">Grupos</p>  
+            </div>    
+        </button></a>
+        <a href="participants.php"><button class="btn_option">
+            <div class="inline_block">
+                <i class="material-icons">emoji_people</i> 
+                <p class="margin_none">Participantes</p>  
+            </div>    
+        </button></a>
+    </div>
+</section>
